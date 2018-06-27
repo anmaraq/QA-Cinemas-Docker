328de47705a0887a9e0620ede2068f837bb56fae
SamHunnable <sam.hunnable@academytrainee.com>
2018-06-27 17:07:44 +0100

    removed most files relating to trainers

--- src/main/java/com/qa/tracker/App.java
+++ src/main/java/com/qa/tracker/App.java
@@ -1,24 +1,16 @@
 package com.qa.tracker;
 
-import com.qa.tracker.entity.Skill;
-import com.qa.tracker.entity.Movie;
-import com.qa.tracker.entity.Trainer;
-import com.qa.tracker.repository.MovieRepository;
-import com.qa.tracker.repository.TrainerRepository;
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
-import java.util.Arrays;
+import com.qa.tracker.entity.Movie;
+import com.qa.tracker.repository.MovieRepository;
 
 @SpringBootApplication
 public class App {
-	
-	@Autowired
-	TrainerRepository trainerRepository;
 	
 	@Autowired
 	MovieRepository movieRepository;
@@ -29,9 +21,7 @@ public class App {
 
 	@PostConstruct
 	public void setupDbWithData(){
-		Trainer trainer= new Trainer("John Gordon", null);
-		trainer.setSkills(Arrays.asList(new Skill("JavaEE"), new Skill("DB")));
-		trainer= trainerRepository.save(trainer);
+
 		
 		Movie film = new Movie("title1","genre1","classification1");
 		film = movieRepository.save(film);
