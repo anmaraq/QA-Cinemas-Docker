11821360e902dd2e6f4e0175cb5fe6e8e8a67821
Admin <Admin@STUDENT08>
2018-06-28 11:54:17 +0100

    added booking table and methods tested by postman

--- src/main/java/com/qa/tracker/App.java
+++ src/main/java/com/qa/tracker/App.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.qa.tracker.entity.Booking;
 import com.qa.tracker.entity.Movie;
+import com.qa.tracker.repository.BookingRepository;
 import com.qa.tracker.repository.MovieRepository;
 
 @SpringBootApplication
@@ -14,6 +16,9 @@ public class App {
 	
 	@Autowired
 	MovieRepository movieRepository;
+	
+	@Autowired
+	BookingRepository bookingRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -25,5 +30,9 @@ public class App {
 		
 		Movie film = new Movie("title1","genre1","classification1");
 		film = movieRepository.save(film);
+		
+		Booking booking = new Booking(1,0,1,1);
+		
+		booking = bookingRepository.save(booking);
 	}
 }
