56228ecb0c13abf964417620de54d7982aa3257e
SamHunnable <samhunnable@academytrainee.com>
2018-06-29 11:43:05 +0100

    Anmars work integrated by Dan

--- client/src/app/components/home-page/home-page.component.ts
+++ client/src/app/components/home-page/home-page.component.ts
@@ -8,8 +8,14 @@ import{Router}  from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 
- jWick:string="http://lwlcdn.lwlies.com/wp-content/uploads/2017/11/John-Wick-2-900x0-c-default.jpg";
+  myLogo:string="http://www.cinemamuseum.org.uk/wordpress/wp-content/uploads/2010/07/CinemaForAllLogo_XtraLarge1-488x256.jpg";
 
+  img1:string="https://www.foxmovies.com/s3/dev-temp/en-US/__5afdb4c667023.jpg"
+
+  img2:string="https://img.youtube.com/vi/20bpjtCbCz0/0.jpg"
+
+  img3:string="https://m.odeon.co.uk/_uploads/asset_management/960x410_17541d8a7c18d04399551bdb1ed8b290.jpg"
+  
   constructor(private _rotuer:Router) { }
 
   ngOnInit() {
