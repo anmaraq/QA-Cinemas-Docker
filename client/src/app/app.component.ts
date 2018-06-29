db3295169f8f66df9e0a584a4430701ba269054f
anmaraq <anmar_aq@hotmail.co.uk>
2018-06-29 09:28:17 +0100

    templatev2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   title = 'app';
 
    myLogo:string="http://www.cinemamuseum.org.uk/wordpress/wp-content/uploads/2010/07/CinemaForAllLogo_XtraLarge1-488x256.jpg";
+
+   img1:string="https://www.foxmovies.com/s3/dev-temp/en-US/__5afdb4c667023.jpg"
+
+   img2:string="https://img.youtube.com/vi/20bpjtCbCz0/0.jpg"
+
+   img3:string="https://m.odeon.co.uk/_uploads/asset_management/960x410_17541d8a7c18d04399551bdb1ed8b290.jpg"
 }
